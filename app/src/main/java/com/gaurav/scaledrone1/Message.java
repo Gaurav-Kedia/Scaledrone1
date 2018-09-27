9c12235567c4036e1b5146610c861d731c0ac0a9
Gaurav kedia <Gaurav.kedia18@gmail.com>
2018-09-27 14:30:49 +0530

    Initial commit

---
+++ app/src/main/java/com/gaurav/scaledrone1/Message.java
@@ -0,0 +1,29 @@
+package com.gaurav.scaledrone1;
+
+/**
+ * Created by GAURAV on 24-06-2018.
+ */
+
+public class Message {
+    private String text; // message body
+    private MemberData data; // data of the user that sent this message
+    private boolean belongsToCurrentUser; // is this message sent by us?
+
+    public Message(String text, MemberData data, boolean belongsToCurrentUser) {
+        this.text = text;
+        this.data = data;
+        this.belongsToCurrentUser = belongsToCurrentUser;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public MemberData getData() {
+        return data;
+    }
+
+    public boolean isBelongsToCurrentUser() {
+        return belongsToCurrentUser;
+    }
+}
